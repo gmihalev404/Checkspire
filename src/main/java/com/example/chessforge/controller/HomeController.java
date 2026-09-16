95f5e17577ae9254e62ff339ebb48140b51dfa00
ГЕРО <gogostanimirov@gmail.com>
2026-09-16 14:39:23 +0300

    feat: display player ratings on home page

--- src/main/java/com/example/chessforge/controller/HomeController.java
+++ src/main/java/com/example/chessforge/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.chessforge.controller;
 
+import com.example.chessforge.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserService userService;
 
     @GetMapping("/")
     public String home(
@@ -16,10 +21,22 @@ public class HomeController {
     ) {
 
         if (principal != null) {
+
             model.addAttribute(
                     "username",
                     principal.getName()
             );
+
+            userService
+                    .findByUsername(
+                            principal.getName()
+                    )
+                    .ifPresent(user ->
+                            model.addAttribute(
+                                    "currentUser",
+                                    user
+                            )
+                    );
         }
 
         return "home";
